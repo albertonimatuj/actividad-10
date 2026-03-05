14963c1b7515bbcbcb60e3f93914e9bb4cd1caf6
ALBERTO NIMATUJ <albertonimuni@gmail.com>
2026-03-04 22:50:14 -0600

    Agregar archivos de proyecto.
    Cambio de estudiantes a productos y listado final
    
    Se reemplazó la gestión de estudiantes por productos, solicitando código y nombre de producto en vez de ID y nombre de estudiante. Se eliminó la búsqueda por ID y ahora se muestra un listado de todos los productos ingresados al finalizar.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Dictionary<int, string> productos = new Dictionary<int, string>();
+
+for (int i = 1; i <= 3; i++)
+{
+    Console.Write("Ingrese el código del producto " + i + ": ");
+    int codigo = int.Parse(Console.ReadLine());
+
+    Console.Write("Ingrese el nombre del producto " + i + ": ");
+    string nombre = Console.ReadLine();
+    productos.Add(codigo, nombre);
+}
+
+Console.WriteLine("\n--- Lista de Productos ---");
+foreach (var item in productos)
+{
+    Console.WriteLine("Código: " + item.Key + " - Producto: " + item.Value);
+}
