2f2eb94e431889838f327cb90161229e33c371b2
ALBERTO NIMATUJ <albertonimuni@gmail.com>
2026-03-04 23:22:10 -0600

    Revert "ejer3ejercicio 3"
    This reverts commit 551c6f1c9894439d11450726b23cdbb7c84d9f7e.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-Dictionary<string, double> notas = new Dictionary<string, double>();
+﻿Dictionary<string, double> notas = new Dictionary<string, double>();
 
 for (int i = 1; i <= 3; i++)
 {
@@ -20,22 +20,4 @@ if (notas.ContainsKey(buscar))
 else
 {
     Console.WriteLine("El estudiante no está registrado.");
-=======
-﻿Dictionary<int, string> productos = new Dictionary<int, string>();
-
-for (int i = 1; i <= 3; i++)
-{
-    Console.Write("Ingrese el código del producto " + i + ": ");
-    int codigo = int.Parse(Console.ReadLine());
-
-    Console.Write("Ingrese el nombre del producto " + i + ": ");
-    string nombre = Console.ReadLine();
-    productos.Add(codigo, nombre);
-}
-
-Console.WriteLine("\n--- Lista de Productos ---");
-foreach (var item in productos)
-{
-    Console.WriteLine("Código: " + item.Key + " - Producto: " + item.Value);
->>>>>>> b134a40516d785b5ce4591b1079387bb800c6f35
 }
