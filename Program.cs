efb3e623bf9db456200eb42cedbb9b436d69e334
ALBERTO NIMATUJ <albertonimuni@gmail.com>
2026-03-04 22:50:14 -0600

    ejer11Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Dictionary<int, string> estudiantes = new Dictionary<int, string>();
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write("Ingrese el ID del estudiante " + i + ": ");
+    int id = int.Parse(Console.ReadLine());
+    while (estudiantes.ContainsKey(id))
+    {
+        Console.WriteLine("El ID ya existe. Ingrese uno diferente.");
+        Console.Write("Ingrese el ID del estudiante " + i + ": ");
+        id = int.Parse(Console.ReadLine());
+    }
+  
+    Console.Write("Ingrese el nombre del estudiante " + i + ": ");
+    string nombre = Console.ReadLine();
+
+    estudiantes.Add(id, nombre);
+
+}
+Console.Write("-------------");
+foreach (var item in estudiantes)
+{
+    Console.WriteLine("ID: " + item.Key + " - Nombre: " + item.Value);
+}
