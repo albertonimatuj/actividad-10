028877c5a3b332a35e465ac5a8992f33d64b3166
ALBERTO NIMATUJ <albertonimuni@gmail.com>
2026-03-04 22:50:14 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Dictionary<int, string> estudiantes = new Dictionary<int, string>();
+
+for (int i = 1; i <= 3; i++)
+{
+    Console.Write("Ingrese el ID del estudiante " + i + ": ");
+    int id = int.Parse(Console.ReadLine());
+    Console.Write("Ingrese el nombre del estudiante " + i + ": ");
+    string nombre = Console.ReadLine();
+    estudiantes.Add(id, nombre);
+
+}
+
+Console.Write("\nIngrese el ID del estudiante que desea buscar: ");
+
+int idABuscar = int.Parse(Console.ReadLine());
+
+if (estudiantes.ContainsKey(idABuscar))
+{
+
+    Console.WriteLine("Resultado: " + estudiantes[idABuscar]);
+
+}
+
+else
+
+{
+
+    Console.WriteLine("El ID no se encuentra en el diccionario.");
+
+}
